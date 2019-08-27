5565186c00e1e624cf48487f2ab4295d790501ec
Constant Dupuis <dev-tools@alterface.com>
2019-08-27 13:43:58 +0200

    Fix range checking issue

--- src/DSP/core/Switch.cpp
+++ src/DSP/core/Switch.cpp
@@ -80,7 +80,7 @@ void pdsp::Switch::process(int bufferSize) noexcept {
         if( state ){ // Changed or AudioRate
             channel = selectBuffer[0];
             if( channel < 0 ) channel = 0;
-            if( channel > (int) inputs.size()) channel = inputs.size()-1;
+            if( channel >= (int) inputs.size()) channel = inputs.size()-1;
         } 
         
         inputs[channel].process();
