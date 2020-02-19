8c1e8ec1e26cd1a23df3aea4afe04ac06c7dc0c3
Nathaniel Potrepka <nspotrepka@gmail.com>
2020-02-18 16:25:41 -0800

    Use static instances vector for ExtSequencer and osc::Input

---
+++ src/messages/ExtSequencer.cpp
@@ -0,0 +1,17 @@
+
+#include "ExtSequencer.h"
+
+std::vector<pdsp::ExtSequencer*> pdsp::ExtSequencer::instances;
+
+pdsp::ExtSequencer::ExtSequencer() {
+    instances.push_back(this);
+}
+
+pdsp::ExtSequencer::~ExtSequencer() {
+    for (size_t i = 0; i < instances.size(); ++i) {
+        if (instances[i] == this) {
+            instances.erase(instances.begin() + i);
+            break;
+        }
+    }
+}
